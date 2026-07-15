75e83045a0d8bf9584b337d9117a012fa90d2af2
Yousef Adel AL-Belbeisi <yousefadelpp@gmail.com>
2026-07-15 13:27:21 +0300

    feat: add About page with corresponding styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from "react-router";
-import FetchApi from "./components/FetchApi";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
 import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<FetchApi />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="product/:id" element={<ProductDetails />} />
+        <Route path="about" element={<About />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/About.css';
+
+function About() {
+  return (
+    <div className="about-container">
+      <h2>About Us</h2>
+      <p>
+        MyStore is a leading e-commerce platform dedicated to providing the best shopping experience. 
+        We source our products globally to ensure high quality and affordable prices.
+      </p>
+      <p>
+        Our mission is to bring joy to our customers through an exceptional variety of products 
+        and top-notch customer service. Thank you for choosing MyStore!
+      </p>
+    </div>
+  );
+}
+
+export default About;
